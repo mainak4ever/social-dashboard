4534288e6e59c2effbdc432488cf28076695b11f
Mainak Mitra <97607911+mainak4ever@users.noreply.github.com>
2024-09-28 03:45:42 +0530

    Created Social Dashboard

---
+++ components/Home/ContactsSidebar/ContactsData.js
@@ -0,0 +1,10 @@
+export const suggestedContacts = [
+    { id: 1, name: 'John Doe', dp: 'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2', requestSent: false },
+    { id: 2, name: 'Jane Smith', dp: 'https://images.pexels.com/photos/1759531/pexels-photo-1759531.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2', requestSent: false },
+    { id: 3, name: 'Alice Johnson', dp: 'https://images.pexels.com/photos/697509/pexels-photo-697509.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2', requestSent: false },
+  ];
+  
+export  const pendingRequests = [
+    { id: 1, name: 'Bob Brown', dp: 'https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=800' },
+    { id: 2, name: 'Charlie Black', dp: 'https://images.pexels.com/photos/428364/pexels-photo-428364.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+  ];
---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ['images.pexels.com',"unsplash.com","plus.unsplash.com","images.unsplash.com"],
+    },
+};
+
+export default nextConfig;
