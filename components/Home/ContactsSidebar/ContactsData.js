e1d593a961a0989a55c436e6f8d1e251edd31024
Mainak Mitra <97607911+mainak4ever@users.noreply.github.com>
2024-09-29 22:08:17 +0530

    Update ContactsData.js

--- components/Home/ContactsSidebar/ContactsData.js
+++ components/Home/ContactsSidebar/ContactsData.js
@@ -8,3 +8,9 @@ export  const pendingRequests = [
     { id: 1, name: 'Bob Brown', dp: 'https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=800' },
     { id: 2, name: 'Charlie Black', dp: 'https://images.pexels.com/photos/428364/pexels-photo-428364.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
   ];
+
+  export const myContactsList = [
+    { id: 1, name: 'Jonny Krant', dp: 'https://images.unsplash.com/photo-1543610892-0b1f7e6d8ac1?q=80&w=1856&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'},
+    { id: 2, name: 'Allen Smith', dp: 'https://images.unsplash.com/photo-1534528741775-53994a69daeb?q=80&w=2864&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
+    { id: 3, name: 'Kobal Dolat', dp: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
+  ];
